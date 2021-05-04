144ebd8131f2c8bc92d1772b38dc290e85e6fcaf
Madimetja <madimetjamampshika@gmail.com>
2021-05-04 18:57:23 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import textwrap
 import pyodbc
 
 
-server = 'tcp:webapplication.database.windows.net'
-database = 'chatapp'
-username = 'ApplicationAdmin'
-password = 'Accounts@madimetja2021'
+server = 'your server name'
+database = 'database name'
+username = 'username'
+password = 'password'
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' +
                       server+';DATABASE='+database+';UID='+username+';PWD=' + password)
 cursor = cnxn.cursor()
